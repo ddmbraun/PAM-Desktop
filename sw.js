152de8875a6c2cacd292c1bf143dbafa8f72503d
ddmbraun <frank.braun@sv-fb.de>
2026-06-02 13:54:40 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pam-desktop-2026-06-02-b223';
+const CACHE_NAME = 'pam-desktop-2026-06-02-b224';
 
 self.addEventListener('install', event => {
   // Kein pre-caching – verhindert addAll-Fehler bei Subdirectory-Deployments (z.B. GitHub Pages)
