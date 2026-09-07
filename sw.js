221518c7f285d579333df152891c209537ee17c2
Frank Braun <frank.braun@sv-fb.de>
2026-09-07 06:32:37 +0200

    Deploy

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pam-desktop-2026-09-06-b923';
+const CACHE_NAME = 'pam-desktop-2026-09-06-b924';
 // Aenderungsnotizen stehen bewusst NICHT hier, sondern lokal in CHANGES.md -
 // diese Datei wird oeffentlich ausgeliefert (b646, Datenschutz).
 
